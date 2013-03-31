b2607d32bff75f4a5e5947808695ebf3e8de7433
Chris Smart <distroguy@gmail.com>
2013-03-31 11:53:20 +1100

    added: argument parsing and setting of variables, printing of usage

--- bmpedit.c
+++ bmpedit.c
@@ -1,4 +1,3 @@
-//comments
 /*
 Copyright 2013 "Christopher Smart" <u3227509@anu.edu.au>
 
@@ -16,13 +15,68 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-//includes
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-//prototypes
+/*prototypes*/
 
-//variables
+/*variables*/
+char output[] = "out.bmp";
+float threshold;
 
-//constants
+/*constants*/
+#define USAGE_STR "This program does simple edits of BMP image files. When the program\n\
+runs it first prints out the width and the height of the input image\n\
+within the BMP file. Once this is done a filter (or sequence of\n\
+filters) are applied to the image. The resulting image is also stored\n\
+using BMP format into an output file."
 
-//functions
+#define OPTIONS_STR "  -o FILE      Sets the output file (defaults to \"out.bmp\").\n\
+  0.0-1.0      Apply this value to threshold filter.\n\
+  -h           Displays this usage message."
+
+/*functions*/
+
+//usage
+void usage(void){
+  printf("\nUsage: bmpedit [OPTIONS...] [input.bmp]\n\n");
+  printf("DESCRIPTION:\n%s\n\n\n", USAGE_STR);
+  printf("OPTIONS:\n%s\n\n", OPTIONS_STR);
+}
+
+//error
+
+//help
+
+int main(int argc, char *argv[]){
+
+  //parse args
+  if (argc < 2){
+    printf("%s: missing file operand\nTry '%s --help' for more information.\n",argv[0],argv[0]);
+    exit(1);
+  }
+  int i;
+  for (i=1;i<argc;i++){
+      if (strcmp(argv[i],"-h") == 0 || strcmp(argv[i],"--help") == 0){
+        usage();
+        exit(0);
+      }else if (strcmp(argv[i],"-o") == 0){
+        printf("output file set to: %s\n", argv[i+1]);
+        printf("output is: %s\n", output);
+        strcpy(output, argv[i+1]);
+        printf("output is now: %s\n", output);
+        i++;
+      }else if (strcmp(argv[i],"-t") == 0){
+        printf("threshold set to: %.1f\n", atof(argv[i+1]));
+        printf("output is: %f\n", threshold);
+        threshold = atof(argv[i+1]);
+        printf("output is now: %f\n", threshold);
+        i++;
+      }else{
+        usage();
+        exit(1);
+     }
+  }
+    return 0;
+}
